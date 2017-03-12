7fecb534880b6c49e9d9fad33fb139d69e19e3ee
theapi <petermclarke@gmail.com>
2017-03-12 21:37:51 +0000

    Show the time

--- examples/Display/Display.ino
+++ examples/Display/Display.ino
@@ -1,4 +1,6 @@
 
+
+
 /** 
  * Set the values of the 6 X 4 seven segment displays via a GET request.
  * 
@@ -13,6 +15,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
+#include <TimeLib.h>
 
 //Max72xxCA_NUM_DIGITS 24
 #include "Max72xxCA.h"
@@ -28,9 +31,29 @@ Denbit denbit;
 
 const byte DEBUG_LED = 16;
 
+unsigned int localPort = 2390;      // local port to listen for UDP packets
+ 
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+ *  Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // NTP server address
+const char* ntpServerName = "uk.pool.ntp.org";
+ 
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+ 
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+
+const long ntp_interval = 60000;
+unsigned long ntp_last = ntp_interval + 1;
+const long seconds_interval = 1000;
+unsigned long seconds_last = seconds_interval + 1;
 
 Max72xxCA display = Max72xxCA();
 ESP8266WebServer server = ESP8266WebServer(80);
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
 
 void setup() {
 
@@ -50,6 +73,11 @@ void setup() {
 
   display.setup(DRIVER_DIN, DRIVER_SCK, DRIVER_LOAD);
 
+
+  Serial.println("Starting UDP");
+  udp.begin(localPort);
+  Serial.print("Local port: ");
+  Serial.println(udp.localPort());
 
   server.on("/data.json", []() {
     // Set the digits.
@@ -91,8 +119,137 @@ void setup() {
 }
 
 void loop() {
+  // Handel web server requests.
   server.handleClient();
+  
   // Check for any Over The Air updates.
   denbit.OTAhandle();
+
+  // Correct the clock time if needed.
+  ntp();
+
+  // Show the time.
+  unsigned long now = millis();
+  if (now - seconds_last >= seconds_interval) {
+    seconds_last = now;
+    displayCurrentTime();
+  }
+}
+
+
+void displayCurrentTime() {
+  byte h = hour();
+  byte m = minute();
+
+  // Hours
+  if (h < 10) {
+    display.setDigitToNumber(24, 10);
+  }
+  else {
+    display.setDigitToNumber(24, h / 10);
+  }
+  display.setDigitToNumber(23, h % 10, true);
+
+  // Minutes
+  if (m < 10) {
+    display.setDigitToNumber(22, 10);
+  }
+  else {
+    display.setDigitToNumber(22, m / 10);
+  }
+  display.setDigitToNumber(21, m % 10);
+  
+  display.update();
+}
+
+void ntp() {
+  static byte awating_packet = 0;
+
+  unsigned long now = millis();
+
+  if (now - ntp_last >= ntp_interval) {
+    ntp_last = now;
+    WiFi.hostByName(ntpServerName, timeServerIP); 
+   
+    sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+    awating_packet = 1;
+  }
+
+  if (awating_packet == 1 && (now - ntp_last > 1000)) {
+    int cb = udp.parsePacket();
+    if (cb) {
+      //Serial.print("packet received, length=");
+      //Serial.println(cb);
+      awating_packet = 0;
+      // We've received a packet, read the data from it
+      udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+   
+      // The timestamp starts at byte 40 of the received packet and is four bytes,
+      // or two words, long. First, esxtract the two words:
+   
+      unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+      unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+      // combine the four bytes (two words) into a long integer
+      // this is NTP time (seconds since Jan 1 1900):
+      unsigned long secsSince1900 = highWord << 16 | lowWord;
+      //Serial.print("Seconds since Jan 1 1900 = " );
+      //Serial.println(secsSince1900);
+   
+      // now convert NTP time into everyday time:
+      //Serial.print("Unix time = ");
+      // Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+      const unsigned long seventyYears = 2208988800UL;
+      // subtract seventy years:
+      unsigned long unixTime = secsSince1900 - seventyYears;
+
+      setTime(unixTime);
+      
+//      // print Unix time:
+//      Serial.println(unixTime);
+//   
+//   
+//      // print the hour, minute and second:
+//      Serial.print("The UTC time is ");       // UTC is the time at Greenwich Meridian (GMT)
+//      Serial.print((unixTime  % 86400L) / 3600); // print the hour (86400 equals secs per day)
+//      Serial.print(':');
+//      if ( ((unixTime % 3600) / 60) < 10 ) {
+//        // In the first 10 minutes of each hour, we'll want a leading '0'
+//        Serial.print('0');
+//      }
+//      Serial.print((unixTime  % 3600) / 60); // print the minute (3600 equals secs per minute)
+//      Serial.print(':');
+//      if ( (unixTime % 60) < 10 ) {
+//        // In the first 10 seconds of each minute, we'll want a leading '0'
+//        Serial.print('0');
+//      }
+//      Serial.println(unixTime % 60); // print the second
+    }
+  }
+
+}
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+  //Serial.println("sending NTP packet...");
+  // set all bytes in the buffer to 0
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  // Initialize values needed to form NTP request
+  // (see URL above for details on the packets)
+  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
+  packetBuffer[1] = 0;     // Stratum, or type of clock
+  packetBuffer[2] = 6;     // Polling Interval
+  packetBuffer[3] = 0xEC;  // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+ 
+  // all NTP fields have been given values, now
+  // you can send a packet requesting a timestamp:
+  udp.beginPacket(address, 123); //NTP requests are to port 123
+  udp.write(packetBuffer, NTP_PACKET_SIZE);
+  udp.endPacket();
 }
 
