eeabf64f4aac89b80a5cca6969a51ba03dbf2478
theapi <petermclarke@gmail.com>
2017-03-19 10:40:10 +0000

    Set a digit to a number

--- alexa/js/index.js
+++ alexa/js/index.js
@@ -18,26 +18,35 @@ var handlers = {
     },
 
     'SetDigitIntent': function () {
-
-        var url = config.url + '?d1=2'; // Proof of concept
-        console.log(url);
-        var that = this;
-        var options = {
-          url: url,
-          rejectUnauthorized: false,
-          agent: false,
-          headers: {
-            'Authorization' : 'Bearer ' + config.token
-          }
-        };
-        request.get(options, function(error, response, body) {
-            //console.log(body);
-            var d = JSON.parse(body);
-            if (d.response.outputSpeech.type = "PlainText") {
-                 that.emit(':tell', d.response.outputSpeech.text);
-             } else {
-                 that.emit(':tell', "Sorry, I could not contact the display");
-             }
-        });
+        var digit = this.event.request.intent.slots.Digit.value;
+        var number = this.event.request.intent.slots.Number.value;
+        if (isNaN(digit)) {
+            this.emit(':tell', `I didn't here a digit. I heard ${digit}`);
+        }
+        else if (isNaN(number)) {
+            this.emit(':tell', `I didn't here a number. I heard ${number}`);
+        }
+        else {
+            var url = config.url + '?d' + digit +  '=' + number;
+            console.log(url);
+            var that = this;
+            var options = {
+              url: url,
+              rejectUnauthorized: false,
+              agent: false,
+              headers: {
+                'Authorization' : 'Bearer ' + config.token
+              }
+            };
+            request.get(options, function(error, response, body) {
+                //console.log(body);
+                var d = JSON.parse(body);
+                if (d.response.outputSpeech.type = "PlainText") {
+                     that.emit(':tell', d.response.outputSpeech.text);
+                 } else {
+                     that.emit(':tell', "Sorry, I could not contact the display");
+                 }
+            });
+        }
     }
  };
